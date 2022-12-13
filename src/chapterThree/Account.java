456cb7989f45960b7f4ec9314ddfff5b131f9965
martha danladi <marthadanladi>
2022-12-13 15:59:43 +0100

    touch

--- src/chapterThree/Account.java
+++ src/chapterThree/Account.java
@@ -1,16 +1,13 @@
 package chapterThree;
 
 public class Account {
-    private String username;
-    public Account(String username) {
-        this.username = username;
+    private String name;
+
+    public void setName(String name){
+        this.name = name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return name;
     }
 }
---
+++ src/chapterThree/AccountTest.java
@@ -0,0 +1,9 @@
+package chapterThree;
+
+import java.util.Scanner;
+
+public class AccountTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
